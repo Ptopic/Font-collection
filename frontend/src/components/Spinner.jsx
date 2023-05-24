5308ebcb13e41df95793fe9a2cbf9aea2910db39
unknown <pingo15102002@gmail.com>
2023-05-25 00:19:21 +0200

    Removed react lottie need to find another package because its broken

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import Lottie from 'react-lottie';
-import spinner from '../assets/images/spinner';
+// import React from 'react';
+// import Lottie from 'react-lottie';
+// import spinner from '../assets/images/spinner';
 
-function Spinner({ size }) {
-	const defaultOptions = {
-		loop: true,
-		autoplay: true,
-		animationData: spinner,
-		rendererSettings: {
-			preserveAspectRatio: 'xMidYMid slice',
-		},
-	};
-	return (
-		<div className="spinner">
-			<Lottie options={defaultOptions} height={size} width={size} />
-		</div>
-	);
-}
+// function Spinner({ size }) {
+// 	const defaultOptions = {
+// 		loop: true,
+// 		autoplay: true,
+// 		animationData: spinner,
+// 		rendererSettings: {
+// 			preserveAspectRatio: 'xMidYMid slice',
+// 		},
+// 	};
+// 	return (
+// 		<div className="spinner">
+// 			<Lottie options={defaultOptions} height={size} width={size} />
+// 		</div>
+// 	);
+// }
 
-export default Spinner;
+// export default Spinner;
